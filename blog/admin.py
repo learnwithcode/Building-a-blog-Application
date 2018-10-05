e6605e208c6fa631191151e470e4cebaf0683638
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-10-05 11:45:38 +0530

    Creating Admin Site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ['title', 'author', 'publish', 'status', 'created', 'updated']
+  list_filter = ['created', 'updated', 'publish']
+  search_fields = ['title', 'body']
+  prepopulated_fields = {'slug':('title',)}
+  raw_id_fields = ('author',)
+  date_hierarchy = 'publish'
+  ordering = ['publish', 'status']
