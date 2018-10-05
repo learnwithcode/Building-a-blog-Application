94be5615c67881ec4c85cf8694dcdb38629d6c7a
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-10-05 11:53:11 +0530

    Building list & detailveiws

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+  posts = Post.published.all()
+  return render(request, "blog/post/list.html", 
+                                            {'posts':posts})
+
+
+def post_detail(request, year, month, day, post):
+  post = get_object_or_404(Post, status='publish',
+                                        publish__year=year,
+                                        publish__month=month,
+                                        publish__day=day,
+                                        slug=post)
+  return render(request, "blog/post/detail.html", 
+                                                {'post':post})    
