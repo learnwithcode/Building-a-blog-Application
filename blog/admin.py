9837328a61db0b073fd5465fdcd145a84c4b469c
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-10-07 09:57:44 +0530

    Model & Admin for Comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -10,3 +10,9 @@ class PostAdmin(admin.ModelAdmin):
   raw_id_fields = ('author',)
   date_hierarchy = 'publish'
   ordering = ['publish', 'status']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+  list_display = ('name', 'email', 'post', 'created', 'updated')
+  list_filter = ('active', 'updated', 'created')
+  search_fields = ('name', 'email', 'body')
