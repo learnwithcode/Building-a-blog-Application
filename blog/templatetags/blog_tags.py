28ad09da6837c927e38dae995a1fee5e082d62ef
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-10-09 19:05:03 +0530

    show_latest_post tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag
 def total_tags():
     return Post.published.count()
+
+@register.inclusion_tag('blog/post/latest_posts.html') 
+def show_latest_post():
+    latest_posts = Post.published.order_by('-publish')[:5]
+    return {'latest_posts':latest_posts}  
