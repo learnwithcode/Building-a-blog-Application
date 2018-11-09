bd2503c1e83bd0c96e040b0686af6a97b516107a
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-11-09 12:03:54 +0530

    Changing site header

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post, Comment
 # Register your models here.
+
+admin.site.site_header = 'blogen'
+admin.site.site_title = 'blogen'
+admin.site.index_title = 'site administration'
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
   list_display = ['title', 'author', 'publish', 'status', 'created', 'updated']
